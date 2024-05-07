8b37c14f502c6c3cce0a2a6ba6e8dac6695434f2
BacaPutera <valachpatrik@gmail.com>
2024-05-07 12:47:26 +0200

    add doku

--- main.py
+++ main.py
@@ -3,8 +3,18 @@ import pandas as pd
 from wikidata.client import Client
 import re
 import os
+from typing import *
 
-def find_translatable_parts(sparql_query):
+def find_translatable_parts(sparql_query: str) -> List[str]:
+    """
+    This function takes a SPARQL query as input and returns a list of all the entities and properties that are translatable.
+
+    Parameters:
+    sparql_query (str): The SPARQL query to be processed
+
+    Returns:
+    List[str]: A list of all the entities and properties that are translatable in the SPARQL query
+    """
     entity_pattern = re.compile(r'wd:Q\d+')
     property_pattern = re.compile(r'wdt:P\d+')
 
@@ -13,7 +23,19 @@ def find_translatable_parts(sparql_query):
 
     return entity_matches + property_matches
 
-def map_wikidata_to_natural_language(sparql_query):
+def map_wikidata_to_natural_language(sparql_query:str) -> str:
+    """
+    This function takes a SPARQL query as input and returns a modified SPARQL query where all the entities and properties that are translatable are replaced with their corresponding natural language labels.
+
+    Parameters:
+    sparql_query (str): The SPARQL query to be processed.
+
+    Returns:
+    str: A modified SPARQL query where all the entities and properties that are translatable are replaced with their corresponding natural language labels.
+
+    Raises:
+    Exception: If an error occurs while retrieving the label of an entity or property and leaves the query as is.
+    """
     client = Client()
     
     translatable_parts = find_translatable_parts(sparql_query)
@@ -28,21 +50,37 @@ def map_wikidata_to_natural_language(sparql_query):
     return sparql_query
     
 
-def load_lc():
+def load_lc() -> pd.DataFrame:
+    """
+    This function loads the LC quad dataset. If the dataset is already locally available, it loads it from the local storage.
+    Otherwise, it fetches the data from the network.
+
+    Parameters:
+    None
+
+    Returns:
+    pd.DataFrame: A pandas DataFrame containing the LC quad dataset.
+    """
     if os.path.exists("lc_quad_translated.csv"):
-        
+        # Load locally if data is already locally ready
         combined_df = pd.read_csv("lc_quad_translated.csv")
         print("Loaded from local")
     else:
+        # Load data from net
         lc_quad_dataset = load_dataset("lc_quad")
 
+        # combine both datasets test and train, we dont need this kind of differentiation
         train_df = lc_quad_dataset["train"].to_pandas()
         test_df = lc_quad_dataset["test"].to_pandas()
-
         combined_df = pd.concat([train_df, test_df], ignore_index=True)
 
+        # Apply KG to map from ambiguous descriptions to NL
         combined_df["wikidata_translated"] = combined_df["sparql_wikidata"].map(map_wikidata_to_natural_language)
         
+        # Delete failed rows
+        combined_df = combined_df[combined_df["wikidata_translated"] != combined_df["sparql_wikidata"]]
+        
+        # Save DF for future use to save resources
         combined_df.to_csv("lc_quad_translated.csv", index=False)
         
         print("Loaded from net")
